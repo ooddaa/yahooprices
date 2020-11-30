5e212a2b6b8e46ef25c65a2bc484276107f98e20
dv <dv@kappacoding.co.uk>
2020-11-30 14:21:28 +0000

    ready for test deploy

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def test():
+    if not request.is_json:
+        return jsonify({ 'error': 'no json body found' }), 400
+    
+    content = request.get_json()
+    return jsonify({ 
+        'ticker': content.get('ticker', 'smth'), 
+        'price': content.get('price', 666), 
+        'date': content.get('date', [2020, 11, 30]), 
+        'time': content.get('time', [14, 57, 0]), 
+        }), 200
+        
+app.run(debug=True)
