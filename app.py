948734a6d5426daf1f9c9a9e85395831edc65cf0
dv <dv@kappacoding.co.uk>
2020-12-01 14:56:39 +0000

    simplify post as google fetch gets 400

--- app.py
+++ app.py
@@ -14,35 +14,38 @@ def response1():
 
 @mamacita.route('/', methods=['POST'])
 def response():
-    if not request.is_json:
-        return jsonify({ 'errors': ['no json body found'] }), 400
+    return jsonify({'post is here': [1,2,3]})
+# @mamacita.route('/', methods=['POST'])
+# def response():
+#     if not request.is_json:
+#         return jsonify({ 'errors': ['no json body found'] }), 400
     
-    data = request.get_json()['data']
+#     data = request.get_json()['data']
 
-    if not data:
-        return jsonify({ 'errors': ['no data received'], 'data': data }), 400
+#     if not data:
+#         return jsonify({ 'errors': ['no data received'], 'data': data }), 400
 
-    # do work
-    new_data = []
-    for item in data:
-        print(item)
-        # valid = []
-        # if not isinstance(item[0], str): valid[0] = False 
-        # else: valid[0] = True
-        # if not isinstance(item[2], list): valid[2] = False 
-        # else: valid[2] = True
-        # if not isinstance(item[3], list): valid[3] = False 
-        # else: valid[3] = True
-        # print(valid)
-        ticker = safe_list_get(item, 0, 'no ticker') 
-        price = safe_list_get(item, 1, 'no price') 
-        date = safe_list_get(item, 2, [])
-        time = safe_list_get(item, 3, [])
-        new_data.append([ticker, price, date, time])
+#     # do work
+#     new_data = []
+#     for item in data:
+#         print(item)
+#         # valid = []
+#         # if not isinstance(item[0], str): valid[0] = False 
+#         # else: valid[0] = True
+#         # if not isinstance(item[2], list): valid[2] = False 
+#         # else: valid[2] = True
+#         # if not isinstance(item[3], list): valid[3] = False 
+#         # else: valid[3] = True
+#         # print(valid)
+#         ticker = safe_list_get(item, 0, 'no ticker') 
+#         price = safe_list_get(item, 1, 'no price') 
+#         date = safe_list_get(item, 2, [])
+#         time = safe_list_get(item, 3, [])
+#         new_data.append([ticker, price, date, time])
     
-    return jsonify({ 
-        'data': new_data
-        }), 200
+#     return jsonify({ 
+#         'data': new_data
+#         }), 200
         
 # app.run(debug=True)
 # mamacita.run(debug=True)
